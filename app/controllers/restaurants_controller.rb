54651434423e64b44373d9dafed3dfb67e120e26
alienk703 <alienk703@gmail.com>
2014-09-02 17:11:51 -0400

    restaurant age limit

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,6 +2,9 @@ class RestaurantsController < ApplicationController
 
 	def index
 		@user = User.find( params[:user_id] )
+		if @user.age >= 20
 		@restaurants = Restaurant.all
+		else @restaurants = Restaurant.where( :serves_alcohol => false)
+		end
 	end
 end
